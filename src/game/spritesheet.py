fafc53f7f738658c37d747584bc349a7d24c2aeb
NickB <32559043+NickB1996@users.noreply.github.com>
2021-10-15 22:14:12 -0600

    Added rotation on creation

--- src/game/spritesheet.py
+++ src/game/spritesheet.py
@@ -5,10 +5,11 @@ class SpriteSheet:
 	def __init__(self, image):
 		self.sheet = image
 
-	def get_image(self, frame, level, width, height, scale, colour):
+	def get_image(self, frame, level, width, height, scale, rotation, colour):
 		image = pygame.Surface((width, height)).convert_alpha()
 		image.blit(self.sheet, (0, 0), ((frame * width), (level * height), width, height))
 		image = pygame.transform.scale(image, (width * scale, height * scale))
+		image = pygame.transform.rotate(image, rotation)
 		image.set_colorkey(colour)
 
 		return image
