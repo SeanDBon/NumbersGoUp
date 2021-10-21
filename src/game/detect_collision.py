e1c17b1a58421b4d4f7374a6c4f266ecb29cff88
NickB <32559043+NickB1996@users.noreply.github.com>
2021-10-20 22:07:54 -0600

    Fixed collision bug that double popped and added sfx

--- src/game/detect_collision.py
+++ src/game/detect_collision.py
@@ -2,24 +2,31 @@ import pygame
 
 
 class CollisionDetection:
-	def __init__(self, weapon_objects, knight_objects):
+	def __init__(self, sound_engine, weapon_objects, knight_objects):
+		self.sound_engine = sound_engine
 		self.weapon_objects = weapon_objects
 		self.knight_objects = knight_objects
+
+		self.check_weapon_collisions()
 
 	# Check if weapons collide with the cursor or a knight
 	def check_weapon_collisions(self):
 		for i, weapon in enumerate(self.weapon_objects):
+			collided = False
 			weapon_rect = weapon.weapon.get_collision_rect()
 			if weapon_rect.collidepoint(pygame.mouse.get_pos()):
-				self.weapon_collided(i)
-			for knight in self.knight_objects:
-				if weapon_rect.colliderect(knight.knight.get_collision_rect()):
-					self.weapon_collided(i)
+				collided = True
+			else:
+				for knight in self.knight_objects:
+					if weapon_rect.colliderect(knight.knight.get_collision_rect()):
+						collided = True
+			if collided:
+				self.weapons_collided(i)
 
-	def weapon_collided(self, i):
-		claimed_weapon = self.weapon_objects.pop(i)
+	def weapons_collided(self, weapon):
+		claimed_weapon = self.weapon_objects.pop(weapon)
+		self.sound_engine.play_sound_effect('pickup')
 
-		# self.pickup_sound.play()
 		# self.total_points += ((weapon.level + 1) * self.point_modifiers[self.weapon_level]) * 10
 		# print(self.claimed_weapons)
 		# if claimed_weapon.weapon.name not in self.claimed_weapons.keys():
