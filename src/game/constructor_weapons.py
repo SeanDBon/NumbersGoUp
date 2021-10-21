19ba5473d9ed2f05b3d2d8cfb16ae3884f770fe4
NickB <32559043+NickB1996@users.noreply.github.com>
2021-10-20 20:23:33 -0600

    Reworked weapon constructed and made weapon animator to follow structure

--- src/game/constructor_weapons.py
+++ src/game/constructor_weapons.py
@@ -1,65 +1,49 @@
 import pygame
 from . import spritesheet
 from random import *
-from settings import Settings
 
 
 class Weapon:
+	def __init__(self, level, weapon_type, position=(0, 0), rotation=0):
+		# sprite is pygame.surface
+		self.sprite = None
 
-	def __init__(self, position=(0, 0)):
-		self.image = None
 		self.name = ''
+		self.level = level
+		self.weapon_type = weapon_type
 
-		self.level = None
-
+		# Default position info
 		self.position = position
-		self.rotation = randint(-360, 360)
+		self.rotation = rotation
 
-		velocity_max = 10
-		velocity_min = -10
-		self.vector_x = uniform(velocity_min, velocity_max)
-		self.vector_y = uniform(velocity_min, velocity_max)
+		# Load sprite sheed and parse with our SpriteSheet class
+		sprite_sheet_image = pygame.image.load('resources/assets/weapons_sprite.png').convert_alpha()
+		self.sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
+
+		self.create_weapon_class()
+
+	def create_weapon_class(self):
+		# Create all weapons types at the specified level
+		weapon_types = ["Sword", "Halberd", "Staff", "Bow", "Shield", "Special"]
+		weapon_levels = ["Wood", "Bronze", "Iron", "Steel", "Obsidian", "Gold", "Diamond", "Zamorak", "Emerald",
+						"Ruby", "Divine", "Void"]
+
+		self.sprite = self.construct_weapon_sprite(self.weapon_type)
+		self.name = weapon_levels[self.level] + ' ' + weapon_types[self.weapon_type]
 
 	def get_collision_rect(self):
-		weapon_rect = self.image.get_rect()
+		weapon_rect = self.sprite.get_rect()
 		x = self.position[0] + 16
 		y = self.position[1] + 16
 		weapon_rect.center = (x, y)
 		return weapon_rect
 
-
-class WeaponsLayer:
-	def __init__(self, level, clutter_amount):
-		sprite_sheet_image = pygame.image.load('resources/assets/weapons_sprite.png').convert_alpha()
-		self.sprite_sheet = spritesheet.SpriteSheet(sprite_sheet_image)
-		self.level = level
-		self.clutter_amount = clutter_amount
-
-	def draw_weapons_layer(self):
-		weapons_layer = []
-		weapon_types = ["Sword", "Halberd", "Staff", "Bow", "Shield", "Special"]
-		weapon_levels = ["Wood", "Bronze", "Iron", "Steel", "Obsidian", "Gold", "Diamond", "Zamorak", "Emerald", "Ruby",
-						"Divine", "Void"]
-
-		clutter = 0
-		while clutter < self.clutter_amount:
-			for weapon_num in range(6):
-				clutter += 1
-				image_loc = weapon_num
-				weapon = Weapon((randint(0, Settings().screen_width-128), randint(0, Settings().screen_height-128)))
-				weapon.image = self.construct_weapon_image(image_loc)
-				weapon.name = weapon_levels[self.level] + ' ' + weapon_types[weapon_num]
-				weapon.level = self.level
-				weapons_layer.append(weapon)
-
-		return weapons_layer
-
-	def construct_weapon_image(self, image_loc):
+	def construct_weapon_sprite(self, image_loc):
 		scale = 2
 		rotation = randint(-360, 360)
 		dim_x = 32
 		dim_y = 32
 		color_key = (0, 0, 0)
 
-		weapon_image = self.sprite_sheet.get_image(image_loc, self.level, dim_x, dim_y, scale, color_key, rotation)
-		return weapon_image
+		weapon_sprite = self.sprite_sheet.get_image(image_loc, self.level, dim_x, dim_y, scale, color_key, rotation)
+		return weapon_sprite
