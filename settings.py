8b2ddec7118673ba8bf6bfc8cf228c9bab36b69a
NickB <32559043+NickB1996@users.noreply.github.com>
2021-10-20 22:07:22 -0600

    Created new sound engine with supporting settings

--- settings.py
+++ settings.py
@@ -4,6 +4,8 @@ import pygame.display
 class Settings:
     screen_height = 1080
     screen_width = 1920
+    music_volume = .09
+    sound_effect_volume = .05
     ACC = 0.5
     FRIC = -0.12
     FPS = 60
